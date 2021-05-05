091f5e599c45816b2e06f38b25fb9fb19cf30b23
宮地宏樹 <hdekm852@gmail.com>
2021-05-05 17:43:18 +0900

    ログインの流れを実装

--- rpa/deposit.py
+++ rpa/deposit.py
@@ -1,13 +1,25 @@
 import time
 from selenium import webdriver
 
+from const.page import page_setting
 
-def deposit(args):
+
+def deposit(data):
     driver = webdriver.Chrome()
-    driver.get('https://www.google.com/')
+    # テスト用
+    pages = page_setting.get('chiho')
+    login_page = pages['login']
+    login_elements = login_page['elements']
+    driver.get(login_page.url)
     time.sleep(5)
-    search_box = driver.find_element_by_name("q")
-    search_box.send_keys('ChromeDriver')
-    search_box.submit()
+
+    id_input = driver.find_elements_by_css_selector(login_elements['id']['selector'])
+    password_input = driver.find_elements_by_css_selector(login_elements['password']['selector'])
+    p_ars_input = driver.find_elements_by_css_selector(login_elements['p_ars']['selector'])
+    id_input.send_keys('12345678')
+    password_input.send_keys('1234')
+    p_ars_input.send_keys('5678')
+    driver.find_elements_by_css_selector(login_elements['login']['selector']).click()
+
     time.sleep(5)
     driver.quit()
