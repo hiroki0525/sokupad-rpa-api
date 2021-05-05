75f8a9f3123fc835320fe41017d1d96b077b979f
宮地宏樹 <hdekm852@gmail.com>
2021-05-05 17:57:41 +0900

    seleniumの処理を別クラスに切り出し

---
+++ infra/sokupad_client.py
@@ -0,0 +1,36 @@
+import time
+
+from selenium import webdriver
+
+from const.page import page_setting
+
+
+class SokupadClient:
+
+    def __init__(self):
+        self.__driver = webdriver.Chrome()
+
+    @staticmethod
+    def wait() -> None:
+        time.sleep(10)
+
+    def get(self, url: str) -> None:
+        return self.__driver.get(url)
+
+    def quit(self) -> None:
+        return self.__driver.quit()
+
+    def login(self, id: str, password: str, p_ars: str) -> None:
+        driver = self.__driver
+        pages = page_setting.get('chiho')
+        login_page = pages['login']
+        login_elements = login_page['elements']
+        driver.get(login_page.url)
+        time.sleep(5)
+        id_input = driver.find_elements_by_css_selector(login_elements['id']['selector'])
+        password_input = driver.find_elements_by_css_selector(login_elements['password']['selector'])
+        p_ars_input = driver.find_elements_by_css_selector(login_elements['p_ars']['selector'])
+        id_input.send_keys(id)
+        password_input.send_keys(password)
+        p_ars_input.send_keys(p_ars)
+        driver.find_elements_by_css_selector(login_elements['login']['selector']).click()
--- rpa/deposit.py
+++ rpa/deposit.py
@@ -1,25 +1,7 @@
-import time
-from selenium import webdriver
-
-from const.page import page_setting
+from infra.sokupad_client import SokupadClient
 
 
 def deposit(data):
-    driver = webdriver.Chrome()
-    # テスト用
-    pages = page_setting.get('chiho')
-    login_page = pages['login']
-    login_elements = login_page['elements']
-    driver.get(login_page.url)
-    time.sleep(5)
-
-    id_input = driver.find_elements_by_css_selector(login_elements['id']['selector'])
-    password_input = driver.find_elements_by_css_selector(login_elements['password']['selector'])
-    p_ars_input = driver.find_elements_by_css_selector(login_elements['p_ars']['selector'])
-    id_input.send_keys('12345678')
-    password_input.send_keys('1234')
-    p_ars_input.send_keys('5678')
-    driver.find_elements_by_css_selector(login_elements['login']['selector']).click()
-
-    time.sleep(5)
-    driver.quit()
+    client = SokupadClient()
+    client.login(data['id'], data['password'], data['p_ars'])
+    client.quit()
