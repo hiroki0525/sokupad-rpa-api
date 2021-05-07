71cee00276c59c32c2e8296b81dcfbb5f7b80ef5
宮地宏樹 <hdekm852@gmail.com>
2021-05-07 22:43:52 +0900

    Pickleでデータを保存するdao追加

--- infra/dao/pickle_rpa_state_dao.py
+++ infra/dao/pickle_rpa_state_dao.py
@@ -1,13 +1,21 @@
+import pickle
+
 from entity.state import RpaState
 from infra.dao import RpaStateDaoInterface
 
 
 class PickleRpaStateDao(RpaStateDaoInterface):
     def insert(self, state: RpaState) -> None:
-        pass
+        with open(state.process_id, mode='wb') as f:
+            pickle.dump(state, f)
 
     def select_by_id(self, process_id: str) -> RpaState:
-        pass
+        with open(process_id, mode='rb') as f:
+            state: RpaState = pickle.load(f)
+        return state
 
     def delete_by_id(self, process_id: str) -> None:
-        pass
+        empty_list = []
+        openfile = open(process_id, 'wb')
+        pickle.dump(empty_list, openfile)
+        openfile.close()
