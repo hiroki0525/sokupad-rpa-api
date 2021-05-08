34139ba5fea7834b5f9ac07141a3edb141dd0cdd
宮地宏樹 <hdekm852@gmail.com>
2021-05-08 21:13:09 +0900

    操作系の処理はsleepするように

--- infra/sokupad_client.py
+++ infra/sokupad_client.py
@@ -9,17 +9,24 @@ import chromedriver_binary
 from const.page import page_setting
 
 
+def dispatch(func, *args, **kwargs):
+    result = func(*args, **kwargs)
+    time.sleep(randrange(2, 5))
+    return result
+
+
 class SokupadClient:
 
-    def __init__(self):
-        self.__driver = webdriver.Chrome()
-
-    @staticmethod
-    def wait() -> None:
-        time.sleep(randrange(5, 10))
+    def __init__(self, headless: bool = False, commit: bool = False):
+        options = None
+        if headless:
+            options = webdriver.ChromeOptions()
+            options.add_argument('--headless')
+        self.__driver = webdriver.Chrome(options=options)
+        self.__commit = commit
 
     def get(self, url: str) -> None:
-        return self.__driver.get(url)
+        return dispatch(self.__driver.get, url)
 
     def quit(self) -> None:
         return self.__driver.quit()
@@ -30,11 +37,10 @@ class SokupadClient:
         login_page = pages['login']
         login_elements = login_page['elements']
         driver.get(login_page.url)
-        time.sleep(5)
         id_input = driver.find_elements_by_css_selector(login_elements['id']['selector'])
         password_input = driver.find_elements_by_css_selector(login_elements['password']['selector'])
         p_ars_input = driver.find_elements_by_css_selector(login_elements['p_ars']['selector'])
-        id_input.send_keys(id)
-        password_input.send_keys(password)
-        p_ars_input.send_keys(p_ars)
-        driver.find_elements_by_css_selector(login_elements['login']['selector']).click()
+        dispatch(id_input.send_keys, id)
+        dispatch(password_input.send_keys, password)
+        dispatch(p_ars_input.send_keys, p_ars)
+        dispatch(driver.find_elements_by_css_selector(login_elements['login']['selector']).click)
