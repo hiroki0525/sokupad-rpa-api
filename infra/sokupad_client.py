02f9a9c78141b266e49d4d749344ad9dddd0729b
宮地宏樹 <hdekm852@gmail.com>
2021-05-09 12:31:49 +0900

    入金まで実行できるように

--- infra/sokupad_client.py
+++ infra/sokupad_client.py
@@ -9,12 +9,6 @@ import chromedriver_binary
 from const.page import page_config
 
 
-def dispatch(func, *args, **kwargs):
-    result = func(*args, **kwargs)
-    time.sleep(randrange(2, 5))
-    return result
-
-
 class SokupadClient:
 
     def __init__(self, headless: bool = False, commit: bool = False):
@@ -24,23 +18,68 @@ class SokupadClient:
             options.add_argument('--headless')
         self.__driver = webdriver.Chrome(options=options)
         self.__commit = commit
+        self.__pages = page_config.get('chiho')
 
     def get(self, url: str) -> None:
-        return dispatch(self.__driver.get, url)
+        return self._dispatch(self.__driver.get, url)
 
     def quit(self) -> None:
         return self.__driver.quit()
 
+    def move_money_account_page(self):
+        driver = self.__driver
+        selector = self.__pages['top']['elements']['money']['selector']
+        button = driver.find_elements_by_css_selector(selector)[0]
+        self._dispatch(button.click)
+        # 別タブで開かれるので切り替える
+        handles = driver.window_handles
+        driver.switch_to.window(handles[len(handles) - 1])
+
+    def move_deposite_page(self):
+        selector = self.__pages['money']['elements']['deposit']['selector']
+        button = self.__driver.find_elements_by_css_selector(selector)[0]
+        self._dispatch(button.click)
+
+    def input_deposit_and_go(self, price):
+        driver = self.__driver
+        deposit_elements = self.__pages['deposit']['elements']
+        deposit_input = driver.find_elements_by_css_selector(deposit_elements['price']['selector'])[0]
+        confirm_button = driver.find_elements_by_css_selector(deposit_elements['confirm']['selector'])[0]
+        self._dispatch(deposit_input.send_keys, price)
+        self._dispatch(confirm_button.click)
+
+    def execute_deposit_and_go(self, password):
+        driver = self.__driver
+        elements = self.__pages['execute_deposit']['elements']
+        password_input = driver.find_elements_by_css_selector(elements['password']['selector'])[0]
+        execute_button = driver.find_elements_by_css_selector(elements['excute']['selector'])[0]
+        self._dispatch(password_input.send_keys, password)
+        self._dispatch(execute_button.click)
+
     def login(self, id: str, password: str, p_ars: str) -> None:
         driver = self.__driver
-        pages = page_config.get('chiho')
+        pages = self.__pages
+
+        # ログイン
         login_page = pages['login']
         login_elements = login_page['elements']
         driver.get(login_page['url'])
         id_input = driver.find_elements_by_css_selector(login_elements['id']['selector'])[0]
         password_input = driver.find_elements_by_css_selector(login_elements['password']['selector'])[0]
         p_ars_input = driver.find_elements_by_css_selector(login_elements['p_ars']['selector'])[0]
-        dispatch(id_input.send_keys, id)
-        dispatch(password_input.send_keys, password)
-        dispatch(p_ars_input.send_keys, p_ars)
-        dispatch(driver.find_elements_by_css_selector(login_elements['login']['selector'])[0].click)
+        self._dispatch(id_input.send_keys, id)
+        self._dispatch(password_input.send_keys, password)
+        self._dispatch(p_ars_input.send_keys, p_ars)
+        self._dispatch(driver.find_elements_by_css_selector(login_elements['login']['selector'])[0].click)
+
+        # お知らせ（あれば）
+        try:
+            notice_ok = driver.find_elements_by_css_selector(pages['notice']['elements']['ok'])[0]
+            self._dispatch(notice_ok.click)
+        except Exception as e:
+            print('No notice page.')
+
+    def _dispatch(self, func, *args, **kwargs):
+        result = func(*args, **kwargs)
+        time.sleep(randrange(2, 5))
+        return result
--- rpa/deposit.py
+++ rpa/deposit.py
@@ -5,4 +5,16 @@ class Deposit(AbstractRpa):
 
     def process(self) -> None:
         params = self._get_params()
-        print(params)
+        client = self._client
+        
+        # 入出金メニュー
+        client.move_money_account_page()
+
+        # 入金ページ
+        client.move_deposite_page()
+
+        # 入金額を入力
+        client.input_deposit_and_go(params.amount)
+
+        # 暗証番号入力
+        client.execute_deposit_and_go(params.user.password)
